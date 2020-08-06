03bb13b9fac6af21a962fcea48049de311912f17
fullbright <full3right@gmail.com>
2020-08-06 17:54:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ usecase = os.environ["INPUT_USECASE"]
 
 dataset_path = "https://raw.githubusercontent.com/" + os.environ["GITHUB_REPOSITORY"] + "/master/" + os.environ["INPUT_DATASET"] + '.csv'
 print("Dataset path {}".format(dataset_path))
+print("Listing current files in the same directory")
+os.listdir()
+
+print("Listing recursively")
+files = []
+path = os.getcwd()
+print("Current working directory : {}".format(path))
+
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.txt' in file:
+            files.append(os.path.join(r, file))
+
+for f in files:
+    print(f)
+    
 data = pd.read_csv(dataset_path)
 data.head()
 
